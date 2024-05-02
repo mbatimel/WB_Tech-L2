06835352f2d8ac0bbbd1ef9fa3ad82c00719dc31
mangushev-ildar <mangushev.ildar@bk.ru>
2024-05-02 22:08:25 +0300

    add golangci-lint 

--- develop/dev01/task.go
+++ develop/dev01/task.go
@@ -3,19 +3,22 @@ package main
 import (
 	"fmt"
 	"os"
+	"time"
 
 	"github.com/beevik/ntp"
 )
 
 func GetTime() {
-	time, err := ntp.Time("0.beevik-ntp.pool.ntp.org")
+	timeNTP, err := ntp.Time("0.beevik-ntp.pool.ntp.org")
+	currentTime := time.Now()
 	if err != nil {
 		fmt.Fprint(os.Stderr, err)
 	} else {
-		fmt.Println(time)
+		fmt.Println("Текущее время:", currentTime)
+		fmt.Println("Точное время (NTP):", timeNTP)
 	}
 }
 
 func main() {
-	GetTime()
+		GetTime()
 }
